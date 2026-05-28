636361f9c1b350d47966b0dae5e5aa0926e3277d
Anasztazia12 <Anasztazia12@users.noreply.github.com>
2026-05-28 17:35:06 +0100

    Add firebase configuration file and update .gitignore to exclude it

---
+++ assets/js/firebase.public.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAc8wfh2Ps4ayv52Furp--ZBdvVaV9K3RE",
+    authDomain: "maths-challenge-23693.firebaseapp.com",
+    projectId: "maths-challenge-23693",
+    storageBucket: "maths-challenge-23693.firebasestorage.app",
+    messagingSenderId: "406061577561",
+    appId: "1:406061577561:web:f70b8f89529743e3e34848",
+    measurementId: "G-SQXN2TCNLM"
+};
